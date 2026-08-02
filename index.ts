79b092dc345081fdb1a3cbc4c4cba9d5832df88b
muradvcv <muradvcv@gmail.com>
2026-08-02 22:27:46 +0600

    edit

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World server running!");
 });
 
 app.listen(port, () => {
